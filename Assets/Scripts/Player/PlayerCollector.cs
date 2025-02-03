1b4566f170ac375a383a2f7fd9bfaf37db679de1
Martin Goldbach <m_goldbach@utb.cz>
2025-02-03 10:45:31 +0100

    pickable objects magnet force fixed

--- Assets/Scripts/Player/PlayerCollector.cs
+++ Assets/Scripts/Player/PlayerCollector.cs
@@ -18,16 +18,18 @@ public class PlayerCollector : MonoBehaviour
         circleCollider = GetComponent<CircleCollider2D>();
     }
 
+
     private void Update()
     {
         // Set the radius of the circle collider to the current pick up range of the player
         circleCollider.radius = player.CurrentPickUpRange;
     }
 
+
     /**
-     * This method is called when the player enters the trigger collider of the object
-     **/   
-    private void OnTriggerEnter2D(Collider2D collision)
+     * OnTriggerStay2D is called when a collider stays inside the trigger collider of the player
+     */
+    private void OnTriggerStay2D(Collider2D collision)
     {
         // Check if the object has the ICollectible interface
         if (collision.gameObject.TryGetComponent(out ICollectible collectable))
@@ -36,6 +38,8 @@ public class PlayerCollector : MonoBehaviour
             Rigidbody2D collectibleRb = collision.gameObject.GetComponent<Rigidbody2D>();
             // Calculate the force direction towards the player (vector from the collectible to the player)
             Vector2 forceDirection = (transform.position - collision.transform.position).normalized;
+            collectibleRb.velocity = Vector2.zero;
+
             // Apply the force to the collectible object to pull it towards the player
             collectibleRb.AddForce(forceDirection * pullSpeed);
             // Call the Collect method
